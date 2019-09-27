e6292c587cd0c799b68574670ae9ae7c6a6e39d0
AXBNBS <44948868+AXBNBS@users.noreply.github.com>
2019-09-27 17:45:48 +0200

    Movimiento y cámara
    El personaje ya se puede mover de manera más cómoda y una cámara básica está hecha ya, falta mirar las colisiones para que no atraviese paredes y un botón para centrarla detrás del personaje.

---
+++ Dream115/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private GameObject followObj, camera, player;
+    private int speed, clampUp, clampDown, inputSensitivity;
+    private float xToPlayer, yToPlayer, zToPlayer, mouseX, mouseY, smoothX, smoothY, rotY, rotX;
+    private Quaternion localRotation;
+
+    
+    // Start is called before the first frame update.
+    private void Start ()
+    {
+        // ACTIVATE IN FINAL VERSION
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+
+        speed = 120;
+        clampUp = +80;
+        clampDown = -30;
+        inputSensitivity = 150;
+        rotX = this.transform.localRotation.eulerAngles.x;
+        rotY = this.transform.localRotation.eulerAngles.y;
+    }
+
+
+    // Update is called once per frame.
+    private void Update ()
+    {
+        mouseX = Input.GetAxis ("Mouse X");
+        mouseY = Input.GetAxis ("Mouse Y");
+        rotY += mouseX * inputSensitivity * Time.deltaTime;
+        rotX += mouseY * inputSensitivity * Time.deltaTime;
+        rotX = Mathf.Clamp (rotX, clampDown, clampUp);
+        localRotation = Quaternion.Euler (rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+
+    // LateUpdate is called after all Update functions have been called.
+    private void LateUpdate ()
+    {
+        this.transform.position = Vector3.MoveTowards (this.transform.position, target.position, speed * Time.deltaTime);
+    }
+}
