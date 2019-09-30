6a07577679429e37b185e4a60f8035d2bb1af0e0
al315233 <al315233@uji.es>
2019-09-30 19:49:08 +0200

    comitttt
    uas

---
+++ Dream115/Assets/Scripts/ObtainingDalsy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObtainingDalsy : MonoBehaviour
+{    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag.Equals("Player"))
+        {
+            ObtainDalsy();
+        }
+    }
+
+    private void ObtainDalsy()
+    {
+        PlayerInteraction.Instance.DalsyCatched();
+
+        //PlayerStatsController player = ; //playerisinvisible
+        Destroy(gameObject);
+    }
+}
--- Dream115/Assets/Scripts/Player/PlayerInteraction.cs
+++ Dream115/Assets/Scripts/Player/PlayerInteraction.cs
@@ -8,10 +8,20 @@ using UnityEngine.UI;
 
 public class PlayerInteraction : MonoBehaviour
 {
+    //Maria
+    public static PlayerInteraction Instance;
+
     public string[] text;
     //public TextElement shownText;
     [SerializeField] private Text shownText;
 
+    //Variables Maria
+    public GameObject player3DModel;
+
+    private void Awake()
+    {
+        Instance = this; //Se guarda la instancia de esto
+    }
 
     // Start is called before the first frame update.
     private void Start ()
@@ -22,7 +32,7 @@ public class PlayerInteraction : MonoBehaviour
 
     // Update is called once per frame.
     private void Update ()
-    {
+    {/*
         if (Input.GetButtonDown ("Interact") == true)
         {
             /*if ()
@@ -32,12 +42,43 @@ public class PlayerInteraction : MonoBehaviour
             else
             {
 
-            }*/
+            }
         }
 
         if (text != null)
         {
             shownText.text = text[0];
-        }
+        }*/
     }
+
+    public void DalsyCatched()
+    {
+        //Llama a la corrutina
+        StartCoroutine(InvisibilizePlayer());
+    }
+
+    //Corrutina
+    IEnumerator InvisibilizePlayer()
+    {
+        float timeInvisible = 5.0f;
+        int parpadeosAux = 0;
+        int numeroParpadeos = 10;
+
+        //playerInvisible = true; //Para los enemigos
+
+        while(parpadeosAux < numeroParpadeos)
+        {
+            player3DModel.SetActive(parpadeosAux%2 == 0);
+
+            yield return new WaitForSeconds(timeInvisible/numeroParpadeos);
+
+            parpadeosAux++;
+
+        }
+
+        //playerInvisible = false; //Para los enemigos
+
+        player3DModel.SetActive(true);
+    }
+
 }
