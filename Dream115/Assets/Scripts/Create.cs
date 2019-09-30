9945f613763d863fd315131eb7cd448988487c23
al315233 <al315233@uji.es>
2019-09-30 14:04:57 +0200

    Bf
    Bf

---
+++ Dream115/Assets/Scripts/Create.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create : InteractableItemBase
+{
+    private bool bebido = false;
+
+    public override void OnInteract()
+    {
+        InteractText = "Press F to drink";
+    }
+}
