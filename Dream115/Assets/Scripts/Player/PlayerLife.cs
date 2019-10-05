d8f789a88c355024723149e97378a56fe9b2ad93
jandrit <al361652@uji.es>
2019-10-05 12:17:07 +0200

    FadeIn, FadeOut y reinicio del nivel hecho
    Pos ya esta

--- Dream115/Assets/Scripts/Player/PlayerLife.cs
+++ Dream115/Assets/Scripts/Player/PlayerLife.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
@@ -10,12 +11,21 @@ public class PlayerLife : MonoBehaviour
     private CharacterController characterCtr;
     private Animator animator;
 
+    public GameObject canvas;
+    private Animator fadeAnimator;
+
+    private Scene scene;
+
     // Start is called before the first frame update
     void Start()
     {
         actualLife = maxLife;
         characterCtr = this.GetComponent<CharacterController>();
         animator = this.GetComponentInChildren<Animator>();
+
+        fadeAnimator = canvas.GetComponentInChildren<Animator>();
+
+        scene = SceneManager.GetActiveScene();
     }
 
     public void TakeDamage(float damage) //Cuando recibes daño
@@ -26,9 +36,22 @@ public class PlayerLife : MonoBehaviour
         {
             animator.SetTrigger("Dead"); //Haces la animacion de morir
             characterCtr.enabled = false; //Desactivas los controles
+            fadeAnimator.SetTrigger("Dead");
+            StartCoroutine(PlayerDead());
         } else //Si no mueres
         {
             animator.SetTrigger("Damaged"); //Haces la animacion de recibir daño
         }
+    }
+
+    IEnumerator PlayerDead()
+    {
+        yield return new WaitForSeconds(3f);
+        RestartScene();
+    }
+
+    private void RestartScene()
+    {
+        SceneManager.LoadScene(scene.name);
     }
 }
