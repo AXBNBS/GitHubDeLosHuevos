b0f9d118139c7e928fad505bc8eaf7e93dfd178e
al315233 <al315233@uji.es>
2019-10-08 10:21:09 +0200

    aaahhhre
    ahre

---
+++ Dream115/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+//using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Scrollbar HealthBar;
+    public float Health = 100;
+
+    public void Damage(float value)
+    {
+        Health -= value;
+        HealthBar.size = Health / 100f;
+    }
+
+}
